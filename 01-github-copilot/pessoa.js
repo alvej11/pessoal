847d31e14bb54c5ec74ba8317ebb263a18396b28
Agnaldo Alves <jose.lima@leega.com.br>
2025-04-30 17:18:54 -0300

    update

--- 01-github-copilot/pessoa.js
+++
@@ -1,8 +0,0 @@
-
-// crie o objeto pessoa com as propriedades nome, idade e cidade
-const pessoa = {
-  nome: "Agnaldo",
-  idade: 100,
-  cidade: "São Paulo"
-};  
-console.log('Resultado...',pessoa);
